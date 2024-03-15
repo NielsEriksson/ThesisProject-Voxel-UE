8c9a6b99e1df25c49d98cc1855238440b5f9b562
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2024-03-15 08:53:21 +0100

    ++

--- ThesisVoxelWorld/Intermediate/Build/BuildRulesProjects/ThesisVoxelWorldModuleRules/obj/Development/ThesisVoxelWorldModuleRules.AssemblyInfo.cs
+++ ThesisVoxelWorld/Intermediate/Build/BuildRulesProjects/ThesisVoxelWorldModuleRules/obj/Development/ThesisVoxelWorldModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThesisVoxelWorldModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e295baf83ccf0804387895928f623dd3bfb1355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6b1ecdc6e2c02edf9b90a1df60c4b47116c797")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThesisVoxelWorldModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThesisVoxelWorldModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
